d700372c1f862497a77c97920e67d67c5923bff0
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-01-21 15:28:31 -0500

    updated file structing for migration and seeds

--- knexfile.js
+++ knexfile.js
@@ -3,13 +3,17 @@
 module.exports = {
   development: {
     client: "sqlite3",
-    useNullAsDefault: true,
+
     connection: {
-      filename: "./dev.sqlite3"
-    }
+      filename: "./data/car-dealer.db3"
+    },
+    useNullAsDefault: true
   },
 
   migrations: {
-    tableName: "knex_migrations"
+    directory: "./data/migrations"
+  },
+  seeds: {
+    directory: "./data/seeds"
   }
 };
