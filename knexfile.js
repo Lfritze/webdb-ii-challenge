68b2425ada05977463e2796417a1cc104cf57cf8
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-01-21 14:48:47 -0500

    knex init setup and setup folders for server and api

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./dev.sqlite3"
+    }
+  },
+
+  migrations: {
+    tableName: "knex_migrations"
+  }
+};
