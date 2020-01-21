3f190e910086a86c2c9c9ecf555038c5878f7e61
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-01-21 18:44:14 -0500

    endpoints working for cars and sales - seeds and migrations working

--- data/seeds/001-cars.js
+++ data/seeds/001-cars.js
@@ -6,22 +6,22 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex("cars").insert([
         {
-          vin: "2G1WF55EXY9103575",
-          make: "Chevrolet",
-          model: "Impala",
-          year: 2000,
+          vin: "ABCDF55EXY9103575",
+          make: "Tesla",
+          model: "Elon-Musker",
+          year: 2050,
           mileage: 110000,
           transmission: "manual",
           title: "clean"
         },
         {
-          vin: "5GAKRDED0CJ396612",
-          make: "Buick ",
-          model: "Enclave",
-          year: 2012,
+          vin: "ZYXYRDED0CJ396612",
+          make: "Delorian ",
+          model: "THe-Doc",
+          year: 1985,
           mileage: 110000,
           transmission: "auto",
-          title: "clean"
+          title: "salvage"
         },
         {
           vin: "WVGAV75N69W510474",
@@ -31,6 +31,15 @@ exports.seed = function(knex) {
           mileage: 110000,
           transmission: "manual",
           title: "clear"
+        },
+        {
+          vin: "GRCDF55EXY9103575",
+          make: "Hot-Dog",
+          model: "Osker-Meyer",
+          year: 2020,
+          mileage: 11440000,
+          transmission: "manual",
+          title: "stolen"
         }
       ]);
     });
